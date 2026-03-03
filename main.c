6db673f31c50324760c72eb759cea7bec88fe517
masamahanok <142673416+masamahanok@users.noreply.github.com>
2026-03-03 16:07:16 +0200

    firs code

---
+++ main.c
@@ -0,0 +1,18 @@
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main() {
+//	double x = 2.07;
+//	double y = 1.55;
+//	double a;
+//
+//	double na = (x + y) * (pow(x, 2) + 4);
+//	double m = exp(x - 2);
+//
+//	a = (pow(1 + y, 2)) * (na / m);
+//
+//	printf("Result a = %.4f", a);
+//	return 0;
+//}
+
+
