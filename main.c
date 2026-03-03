4a6d6963ab31fb7eb1bef151ff14a49defd1fb65
masamahanok <142673416+masamahanok@users.noreply.github.com>
2026-03-03 16:17:46 +0200

    second trie

--- main.c
+++ main.c
@@ -15,4 +15,27 @@
 //	return 0;
 //}
 
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+	
+	double a, x, y;
+
+	printf("x:");
+	scanf("%lf", &x);
+
+	printf("y:");
+	scanf("%lf", &y);
+
+	double na = (x + y) * (pow(x, 2) + 4);
+	double m = exp(x - 2);
+
+	a = (pow(1 + y, 2)) * (na / m);
+
+	
+
+	printf("Result a = %.4f", a);
+	return 0;
+}
